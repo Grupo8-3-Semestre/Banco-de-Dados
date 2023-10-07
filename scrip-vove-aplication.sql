c358ff09a6db80022d1491e9d4a1fee3a2ee323e
Luiz Felipe <luiz.nunes@sptech.school>
2023-10-07 19:26:36 -0300

    script atualizado para subir foto

--- scrip-vove-aplication.sql
+++ scrip-vove-aplication.sql
@@ -1,8 +1,4 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
 
-/* para workbench - local - desenvolvimento */
 
 CREATE DATABASE vove;
 USE vove;
@@ -24,6 +20,7 @@ CREATE TABLE usuario (
     cep VARCHAR(10),
     data_nasc DATE,
     email VARCHAR(65),
+    foto_perfil VARCHAR(255),
     genero_preferido VARCHAR(255),
     logado BOOLEAN,
     nome VARCHAR(32),
@@ -75,12 +72,12 @@ SELECT * FROM votacao;
 
 
 -- Inserção de dados na tabela "usuario"
-INSERT INTO usuario (aceita_email, aparelho_utilizado, assina_streaming, busca_avaliacao, cep, data_nasc, email, genero_preferido, logado, nome, qtd_frequencia, senha, sexo)
+INSERT INTO usuario (aceita_email, aparelho_utilizado, assina_streaming, busca_avaliacao, cep, data_nasc, email,foto_perfil, genero_preferido, logado, nome, qtd_frequencia, senha, sexo)
 VALUES
- (TRUE, 'Celular', TRUE, FALSE, '12345', '2000-01-01', 'john.doe@gmail.com', 'Ação', FALSE, 'John Doe', 10, '$2a$10$PU4XokmGPNejARzCnblVD.dWzdX3eCvb5pY6.5qh4YYqSNvirQKsq', 'Masculino'),
- (FALSE, 'Celular', TRUE, FALSE, '12345', '2000-01-01', 'luiz@vove.com', 'rock', FALSE, 'Luize', 10, '$2a$10$PU4XokmGPNejARzCnblVD.dWzdX3eCvb5pY6.5qh4YYqSNvirQKsq', 'Masculino'),
- (TRUE, 'Tablet', FALSE, TRUE, '54321', '1995-07-15', 'ana@gmail.com', 'Comédia', FALSE, 'Ana Silva', 10, '$2a$10$PU4XokmGPNejARzCnblVD.dWzdX3eCvb5pY6.5qh4YYqSNvirQKsq', 'Feminino'),
- (FALSE, 'Computador', FALSE, FALSE, '98765', '1988-03-10', 'carlos@gmail.com', 'Ação', FALSE, 'Carlos Santos', 10, '$2a$10$PU4XokmGPNejARzCnblVD.dWzdX3eCvb5pY6.5qh4YYqSNvirQKsq', 'Masculino');
+ (TRUE, 'Celular', TRUE, FALSE, '12345', '2000-01-01', 'john.doe@gmail.com','/foto-usuario', 'Ação', FALSE, 'John Doe', 10, '$2a$10$PU4XokmGPNejARzCnblVD.dWzdX3eCvb5pY6.5qh4YYqSNvirQKsq', 'Masculino'),
+ (FALSE, 'Celular', TRUE, FALSE, '12345', '2000-01-01', 'luiz@vove.com','/foto-usuario', 'rock', FALSE, 'Luize', 10, '$2a$10$PU4XokmGPNejARzCnblVD.dWzdX3eCvb5pY6.5qh4YYqSNvirQKsq', 'Masculino'),
+ (TRUE, 'Tablet', FALSE, TRUE, '54321', '1995-07-15', 'ana@gmail.com','/foto-usuario', 'Comédia', FALSE, 'Ana Silva', 10, '$2a$10$PU4XokmGPNejARzCnblVD.dWzdX3eCvb5pY6.5qh4YYqSNvirQKsq', 'Feminino'),
+ (FALSE, 'Computador', FALSE, FALSE, '98765', '1988-03-10', 'carlos@gmail.com','/foto-usuario', 'Ação', FALSE, 'Carlos Santos', 10, '$2a$10$PU4XokmGPNejARzCnblVD.dWzdX3eCvb5pY6.5qh4YYqSNvirQKsq', 'Masculino');
 
 -- Inserção de dados na tabela "comentario"
 INSERT INTO comentario (descricao, momento_comentario, tmdb_id_filme, usuario_id, spoiler, avaliacao)
@@ -121,56 +118,3 @@ SELECT * FROM comentario;
 SELECT * FROM lista;
 SELECT * FROM lista_tabela;
 SELECT * FROM votacao;
-
-
-
-
-
-
-
-
-
-/* para sql server - remoto - produção */
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(32),
-    email VARCHAR(65),
-    senha VARCHAR(255),
-    data_nasc DATE,
-    aceita_email BIT
-);
-
-CREATE TABLE arquivo (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome_arquivo_original VARCHAR(255),
-    nome_arquivo_salvo VARCHAR(255),
-    dados VARBINARY(100),
-    data_upload DATETIME DEFAULT GETDATE()
-); 
-
-CREATE TABLE comentario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	descricao VARCHAR(1000),
-    momento_comentario DATETIME DEFAULT GETDATE(),
-	usuario_id INT FOREIGN KEY REFERENCES usuario(id)
-);
-CREATE TABLE lista (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome_da_lista VARCHAR(100)
-); 
-CREATE TABLE lista_tabela (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	usuario_id INT FOREIGN KEY REFERENCES usuario(id),
-	lista_filme_id INT FOREIGN KEY REFERENCES lista(id),
-    tmdb_id_filme INT
-);
-
-CREATE TABLE votacao (
-	id INT PRIMARY KEY IDENTITY(1,1),
-    avaliacao INT,
-    usuario_id INT FOREIGN KEY REFERENCES usuario(id),
-    tmdb_id_filme INT);
-
-
-
